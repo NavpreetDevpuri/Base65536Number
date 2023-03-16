5e7c7241d834ec0350d5d298011b20765c07df84
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2023-03-16 14:07:16 +0530

    Refcatored length to maxLength

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export default class Base65536Number {
-  constructor(digits, length = 50) {
-    if (digits.length > length) {
+  constructor(digits, maxLength = 50) {
+    if (digits.length > maxLength) {
       throw new Error('Digits length is too long');
     }
     const base = 65536;
@@ -8,11 +8,11 @@ export default class Base65536Number {
     for (let i = 0; i < base; i++) {
       this.charset[i] = String.fromCharCode(i);
     }
-    this.length = length;
+    this.maxLength = maxLength;
     this.digits = digits;
     if (typeof digits == 'string') {
-      this.digits = new Uint16Array(this.length);
-      for (let i = digits.length, j = length; i > -1; i--, j--) {
+      this.digits = new Uint16Array(this.maxLength);
+      for (let i = digits.length, j = maxLength; i > -1; i--, j--) {
         this.digits[j] = digits.charCodeAt(i);
       }
     }
@@ -20,7 +20,7 @@ export default class Base65536Number {
 
   toString() {
     let result = '';
-    for (let i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.maxLength; i++) {
       result += this.charset[this.digits[i]];
     }
     return result;
@@ -30,9 +30,9 @@ export default class Base65536Number {
     if (this.toString() < other.toString()) {
       throw new Error('Subtraction result is negative');
     }
-    const result = new Uint16Array(this.length);
+    const result = new Uint16Array(this.maxLength);
     let carry = 0;
-    for (let i = this.length - 1; i > -1; i--) {
+    for (let i = this.maxLength - 1; i > -1; i--) {
       if (this.digits[i] < other.digits[i]) {
         result[i] = this.digits[i] - carry + 65536 - other.digits[i];
         carry = 1;
@@ -41,33 +41,33 @@ export default class Base65536Number {
       result[i] = this.digits[i] - carry - other.digits[i];
       carry = 0;
     }
-    return new Base65536Number(result, this.length);
+    return new Base65536Number(result, this.maxLength);
   }
 
   add(other) {
-    const result = new Uint16Array(this.length);
+    const result = new Uint16Array(this.maxLength);
     let carry = 0;
-    for (let i = this.length - 1; i > -1; i--) {
+    for (let i = this.maxLength - 1; i > -1; i--) {
       const curr = this.digits[i] + other.digits[i] + carry;
       carry = curr > 65535 ? 1 : 0;
       result[i] = curr;
     }
-    return new Base65536Number(result, this.length);
+    return new Base65536Number(result, this.maxLength);
   }
 
   half() {
-    const result = new Uint16Array(this.length);
+    const result = new Uint16Array(this.maxLength);
     let carry = 0;
-    for (let i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.maxLength; i++) {
       const curr = (this.digits[i] >> 1) | (carry << 15);
       carry = curr & 1 ? 1 : 0;
       result[i] = curr;
     }
-    return new Base65536Number(result, this.length);
+    return new Base65536Number(result, this.maxLength);
   }
 
   average(other) {
-    if (this.length != other.length) {
+    if (this.maxLength != other.length) {
       throw new Error('Operands are not of the same length');
     }
     let bothOddFlag = 0;
@@ -81,7 +81,7 @@ export default class Base65536Number {
     const bHalf = other.half();
     const result = thisHalf
       .add(bHalf)
-      .add(new Base65536Number(bothOddFlag.toString(), this.length));
+      .add(new Base65536Number(bothOddFlag.toString(), this.maxLength));
     return result;
   }
 }
